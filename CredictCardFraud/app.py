e3e1048da2f238dc7366e6fa43d3357c5695f792
Pradeepti23 <sridevimitra1975@gmail.com>
2026-06-20 06:48:03 +0530

    Fix OTP email using Resend API and improve Render deployment

--- CredictCardFraud/app.py
+++ CredictCardFraud/app.py
@@ -85,28 +85,35 @@ except Exception as e:
 # ==========================================================
 # EMAIL OTP
 # ==========================================================
+import requests
 
 def send_otp_email(receiver_email, otp):
-    sender_email = os.environ.get("EMAIL_USER")
-    app_password = os.environ.get("EMAIL_PASS")
 
-    msg = MIMEText(f"Your OTP is: {otp}")
-    msg["Subject"] = "OTP Verification"
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
+    api_key = os.environ.get("RESEND_API_KEY")
+
+    if not api_key:
+        print("RESEND API KEY NOT FOUND")
+        return
 
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587, timeout=10)
-        server.starttls()
-        server.login(sender_email, app_password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        server.quit()
+        response = requests.post(
+            "https://api.resend.com/emails",
+            headers={
+                "Authorization": f"Bearer {api_key}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "from": "onboarding@resend.dev",
+                "to": receiver_email,
+                "subject": "OTP Verification",
+                "text": f"Your OTP is: {otp}"
+            }
+        )
+
+        print("EMAIL RESPONSE:", response.text)
+
     except Exception as e:
         print("Email Error:", e)
-
-
-def send_otp_email_async(email, otp):
-    threading.Thread(target=send_otp_email, args=(email, otp)).start()
 
 # ==========================================================
 # HOME
